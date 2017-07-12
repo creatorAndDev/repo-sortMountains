dab2efd347dcea9c0247a94ce817c29899068c91
Yuri <creator.and.dev@gmail.com>
2017-07-12 18:05:08 +0300

    complate!

--- src/sortMountains.java
+++ src/sortMountains.java
@@ -1,9 +1,56 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 
 public class sortMountains {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	LinkedList <Mountain> mtn = new LinkedList<Mountain>();
+	
+	class NameCompare implements Comparator <Mountain> {
+		public int compare(Mountain one, Mountain two) {
+			return one.name.compareTo(two.name);
+		}
+	}
+	
+	class HeightCompare implements Comparator <Mountain> {
+		public int compare(Mountain one, Mountain two) {
+			return (two.height - one.height);
+		}
 	}
 
+	public static void main(String[] args) {
+		new sortMountains().go();
+	}
+
+	public void go() {
+		mtn.add(new Mountain("Лонг-Рейдж", 14255));
+		mtn.add(new Mountain("Эльберт", 14433));
+		mtn.add(new Mountain("Марун", 14156));
+		mtn.add(new Mountain("Касл", 14265));
+		
+		System.out.println("В порядке добавления: \n" + mtn);
+		System.out.println();
+		
+		NameCompare nc = new NameCompare();
+		Collections.sort(mtn, nc);
+		System.out.println("По названию: \n" + mtn);
+		System.out.println();
+		
+		HeightCompare hc = new HeightCompare();
+		Collections.sort(mtn, hc);
+		System.out.println("По высоте: \n" + mtn);
+	}
+	
+	class Mountain {
+		String name;
+		int height;
+		
+		Mountain(String n, int h){
+			name = n;
+			height = h;
+		}
+		
+		public String toString() {
+			return name + " " + height;
+		}
+	}
 }
